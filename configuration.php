f75634e18f5ef090d255d33cf94c018d80fd77a3
mebingj <63694408+mebingj@users.noreply.github.com>
2020-04-15 13:50:59 +1000

    Add files via upload

---
+++ configuration.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Here all common required details related to account will be stored
+ */
+
+define('API_KEY', '5O6tqjHLT8eor59kzrmekA');
+
+define('API_SECRET', 'XgcFlgJilGUinaUepMmtedG2aBVTGWrb');
+
+define('API_URL', 'https://api.zoom.us/v2/');
+
+?>
